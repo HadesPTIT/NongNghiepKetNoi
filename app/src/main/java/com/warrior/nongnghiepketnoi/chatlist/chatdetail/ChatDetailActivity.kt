3b6c52e3b54bbec6214b27ddbe713d1bcfc79d14
NamTB <NA>
2019-10-19 19:24:16 +0700

    handleloading

--- app/src/main/java/com/warrior/nongnghiepketnoi/chatlist/chatdetail/ChatDetailActivity.kt
+++ app/src/main/java/com/warrior/nongnghiepketnoi/chatlist/chatdetail/ChatDetailActivity.kt
@@ -2,7 +2,8 @@ package com.warrior.nongnghiepketnoi.chatlist.chatdetail
 
 import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.view.View
 import com.warrior.nongnghiepketnoi.R
 import kotlinx.android.synthetic.main.activity_chat_detail.*
 
@@ -10,6 +11,9 @@ class ChatDetailActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_detail)
+        Handler().postDelayed({
+            progress_loading?.visibility = View.GONE
+        }, 500L)
         back_button.setOnClickListener { onBackPressed() }
     }
 }
