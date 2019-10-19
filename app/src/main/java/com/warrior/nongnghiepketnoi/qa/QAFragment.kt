e531372b59f6796e3888d37b16ea93e27f25a5a9
hoanglong96 <long11hq@gmail.com>
2019-10-19 22:49:30 +0700

    Refs task update quiz

--- app/src/main/java/com/warrior/nongnghiepketnoi/qa/QAFragment.kt
+++ app/src/main/java/com/warrior/nongnghiepketnoi/qa/QAFragment.kt
@@ -14,6 +14,7 @@ import com.warrior.nongnghiepketnoi.R
 import com.warrior.nongnghiepketnoi.constance.Const
 import com.warrior.nongnghiepketnoi.newfeeds.AdapterFeed
 import com.warrior.nongnghiepketnoi.newfeeds.ModelFeed
+import kotlinx.android.synthetic.main.fragment_new_feedsqa.*
 
 
 private const val ARG_PARAM1 = "param1"
